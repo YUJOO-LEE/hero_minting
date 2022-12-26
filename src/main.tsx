195f215a34551c78f10daef1cbe878120caf7505
YUJOO-LEE <lllllllllee@gmail.com>
2022-12-27 03:49:21 +0900

    지갑 연결 및 민팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+const getLibrary = (provider: any) => {
+  console.log('[getLibrary] provider', provider);
+  return new Web3Provider(provider);
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
-)
+);
