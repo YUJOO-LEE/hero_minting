d2be0ad4b37ca961daa5a78838bfd78f00a22654
YUJOO-LEE <lllllllllee@gmail.com>
2022-12-27 04:41:35 +0900

    chakra-ui 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,83 @@
-import Home from './components/Home';
+import { Box, Button, ButtonGroup, Flex, Text } from '@chakra-ui/react';
+import { useWeb3React } from '@web3-react/core';
+import { useState } from 'react';
+import { Injected } from './lib/connectors';
+import { ContractInstance } from './lib/contract';
 
 const App = () => {
+  const [isMinting, setIsMinting] = useState(false);
+  
+  const {
+    library,  // web3 provider 제공
+    chainId,  // dapp에 연결된 account의 chainId
+    account,  // dapp에 연결된 account address
+    active, // dapp 유저가 로그인 된 상태인지 체크
+    activate, // dapp 월렛 연결 기능 수행 함수
+    deactivate, // dapp 월렛 연결 해제 수행 함수
+  } = useWeb3React();
+
+  const handleConnect = () => {
+    if ((window as any).ethereum === undefined) {
+      // 지갑 미설치 상태라면 설치 페이지 오픈
+      window.open(
+        `https://metamask.app.link/dapp/${window.location.host}`,
+        '_blank'
+      );
+      return;
+    }
+    if (active && account) {
+      deactivate();
+      // 연결되어있는 상태라면 연결해제
+    }
+    activate(Injected);
+    // Injected 넘겨줌
+  }
+
+  const handleMint = async () => {
+    try{
+
+      const data = await ContractInstance.populateTransaction.summon();
+      setIsMinting(true);
+      const signer = library.getSigner(); // 계정 잠금해제
+      const signedTransaction = await signer.sendTransaction(data);
+      // 잠금해제된 계정으로, 트랜잭션 구조 객체를 담아서 트랜잭션 전송
+      let reciept = await signedTransaction.wait();
+      console.log('reciept', reciept);
+      setIsMinting(false);
+
+    } catch(error) {
+
+      console.log('error!', error);
+      setIsMinting(false);
+    }
+  };
+
   return (
-    <div>
-      <Home />
-    </div>
+    <Flex w='100%' h='100vh' direction='column' justifyContent='center' alignItems='center'>
+      {account &&
+        <Box mb={10}>
+          <Text>Account: {account}</Text>
+          <Text>chainId: {chainId}</Text>
+        </Box>
+      }
+      <ButtonGroup>
+        <Button onClick={handleConnect}
+          colorScheme={active ? 'gray' : 'blue'}
+        >
+          {active ? '연결 해제' : '지갑 연결하기'}
+        </Button>
+
+      {account &&
+        <Button
+          onClick={handleMint}
+          disabled={isMinting === true}
+          colorScheme='blue'
+        >
+          Minting
+        </Button>
+      }
+      </ButtonGroup>
+    </Flex>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const getLibrary = (provider: any) => {
   console.log('[getLibrary] provider', provider);
@@ -12,7 +13,9 @@ const getLibrary = (provider: any) => {
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Web3ReactProvider>
   </React.StrictMode>,
 );
